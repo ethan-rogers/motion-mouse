3569e8fbe069ed9d1c0e7d3ab3c514e19a155868
Ethan Rogers <75224466+holt-rogers@users.noreply.github.com>
2025-12-17 15:03:53 -0500

    switched frrom analog to i2c communciation for sensor

--- mouse_demo/mouse_demo.ino
+++ mouse_demo/mouse_demo.ino
@@ -1,4 +1,5 @@
 #include "Mouse.h"
+#include "Wire.h" 
 
 // input
 
@@ -13,10 +14,23 @@
 #define LEFT_LED 30
 #define RIGHT_LED 17
 
+const int MPU_ADDR = 0x68;
+
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; 
+int16_t gyro_x, gyro_y, gyro_z;
+int16_t temperature; 
+
 // variables to get change in rotation
 int xLast;
 int yLast;
 int zLast;
+
+char tmp_str[7]; 
+
+char* convert_int16_to_str(int16_t i) { 
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
 
 // calibration for the sensor
 void setup() {
@@ -28,9 +42,11 @@ void setup() {
   pinMode(LEFT_CLICK, INPUT_PULLUP);
   pinMode(RIGHT_CLICK, INPUT_PULLUP);
 
-  pinMode(X_OUT, INPUT);
-  pinMode(Y_OUT, INPUT);
-  pinMode(Z_OUT, INPUT);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); 
+  Wire.write(0x6B);
+  Wire.write(0); 
+  Wire.endTransmission(true);
 
 
   // output lights
@@ -72,40 +88,29 @@ void loop() {
     digitalWrite(RIGHT_LED, HIGH);
   }
 
-// Read the raw analog values from the sensor pins
-  int xValue = analogRead(X_OUT);
-  int yValue = analogRead(Y_OUT);
-  int zValue = analogRead(Z_OUT);
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); 
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 7*2, true); 
+  
 
+  accelerometer_x = Wire.read()<<8 | Wire.read();
+  accelerometer_y = Wire.read()<<8 | Wire.read();
+  accelerometer_z = Wire.read()<<8 | Wire.read(); 
+  temperature = Wire.read()<<8 | Wire.read();
+  gyro_x = Wire.read()<<8 | Wire.read(); 
+  gyro_y = Wire.read()<<8 | Wire.read(); 
+  gyro_z = Wire.read()<<8 | Wire.read();
+  
 
-  int minVal = 1;
-  int xMove = 0;
-  int yMove = 0;
+  Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
+  Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
+  Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
 
-  int moveMultiplier = 5;
-
-  if ((yValue - yLast) > minVal || (yValue - yLast)*-1 > minVal){
-    xMove = (yValue - yLast)*moveMultiplier;
-  }
-
-  if ((xValue - xLast) > minVal || (yValue - yLast)*-1 > minVal){
-    yMove = -(xValue - xLast)*moveMultiplier;
-  }
-
-  Mouse.move(xMove, yMove);
-
-  xLast = xValue;
-  yLast = yValue;
-  zLast = zValue;
-  // Print the values to the Serial Monitor
-  //Serial.print("X: ");
-  //Serial.print(xValue);
-  //Serial.print(" | Y: ");
-  //Serial.print(yValue);
-  //Serial.print(" | Z: ");
-  //Serial.println(zValue);
-
-  // Small delay for stable readings
-  //delay(100);
+  Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
+  Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
+  Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
+  Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
+  Serial.println();
 
 }
